3dc9b77245bc00196e9c29cbef05a1d12f089401
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2024-06-27 06:08:42 -0700

    chore: Update sections for jake

--- mp3tagger/data/sections.py
+++ mp3tagger/data/sections.py
@@ -1,12 +1,177 @@
 from .constants import *
 
 
-sections_data = [
+
+for_jake = [
     {
         "group_control": NEUTRAL,
         "label": "cowriter",
         "scales": [
-            ("None", NEUTRAL),
+            ("None", ON),
+            ("track", NEUTRAL),
+            ("cowriter", NEUTRAL),
+            # ("2", NEUTRAL),
+            # ("1", NEUTRAL),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "composer",
+        "scales": [
+            ("Rivers Cuomo", ON),
+            # ("track", NEUTRAL),
+            # ("cowriter", NEUTRAL),
+            # ("2", NEUTRAL),
+            # ("1", NEUTRAL),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "jd",
+        "scales": [
+            ("5", NEUTRAL),
+            ("4", NEUTRAL),
+            ("3", NEUTRAL),
+            ("2", NEUTRAL),
+            ("1", NEUTRAL),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "jd",
+        "scales": [
+            ("5", NEUTRAL),
+            ("4", NEUTRAL),
+            ("3", NEUTRAL),
+            ("2", NEUTRAL),
+            ("1", NEUTRAL),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "Genesis",
+        "scales": [
+            ("piano", NEUTRAL),            
+            ("track", NEUTRAL),            
+            ("aku", NEUTRAL),
+            ("dist", NEUTRAL),
+            ("melody", NEUTRAL),
+            ("clean", NEUTRAL),
+            ("lyric-piano", OFF),
+            ("lyric", OFF),
+            ("title", OFF),
+            ("mouth", OFF),
+            ("beat", OFF),
+
+        ],
+    },
+        {
+        "group_control": NEUTRAL,
+        "label": "Masterpiece",
+        "scales": [
+            ("5", NEUTRAL),
+            ("4", NEUTRAL),
+            ("3", NEUTRAL),
+            ("2", OFF),
+            ("1", OFF),
+        ],
+    },
+    # {
+    #     "group_control": NEUTRAL,
+    #     "label": "Section",
+    #     "scales": [
+    #         ("chorus", NEUTRAL),
+    #         ("verse", NEUTRAL),
+    #         ("pre", NEUTRAL),
+    #         ("post", NEUTRAL),
+    #         ("bridge", NEUTRAL),
+    #         ("instr", NEUTRAL),
+    #         ("riff", NEUTRAL),
+    #         ("beat", OFF),
+    #     ],
+    # },
+    # {
+    #     "group_control": NEUTRAL,
+    #     "label": "Feel",
+    #     "scales": [
+    #         ("2", NEUTRAL),
+    #         ("3", NEUTRAL),
+    #         ("4", NEUTRAL),
+    #         ("6", NEUTRAL),
+    #         ("shuffle", NEUTRAL),
+    #         # ("half", OFF),
+    #         ("waltz", NEUTRAL),
+    #         ("triplet", NEUTRAL),
+    #         # ("straight", NEUTRAL),
+    #     ],
+    # },
+    # {
+    #     "group_control": NEUTRAL,
+    #     "label": "FirstChord",
+    #     "scales": [
+    #         ("1", NEUTRAL),
+    #         ("6", NEUTRAL),
+    #         ("4", NEUTRAL),
+    #         ("5", NEUTRAL),
+    #         ("3", NEUTRAL),
+    #     ],
+    # },
+    # {
+    #     "group_control": NEUTRAL,
+    #     "label": "Melody",
+    #     "scales": [
+    #         ("3", NEUTRAL),
+    #         ("1", NEUTRAL),
+    #         ("6", NEUTRAL),
+    #         ("4", NEUTRAL),
+    #         ("2", NEUTRAL),
+    #         ("5", NEUTRAL),
+    #         ("7", NEUTRAL),
+    #     ],
+    # },
+    # {
+    #     "group_control": NEUTRAL,
+    #     "label": "Mode",
+    #     "scales": [
+    #         ("minor", NEUTRAL),
+    #         ("major", NEUTRAL),
+    #         ("mixo", OFF),
+    #         ("mixed", OFF),
+    #         ("phrygian", NEUTRAL),
+    #         ("lydian", NEUTRAL),
+    #     ],
+    # },
+    # {
+    #     "group_control": NEUTRAL,
+    #     "label": "melodyStart",
+    #     "scales": [
+    #         ("before", NEUTRAL),
+    #         ("on", NEUTRAL),
+    #         ("after", NEUTRAL),
+    #     ],
+    # },
+    {
+        "group_control": NEUTRAL,
+        "label": "%_folderpath%",
+        "scales": [
+            ("ideas", OFF),
+            ("solo", OFF),
+            ("covers", OFF),
+            ("previous", OFF),
+            ("hold", OFF),
+            ("The Pink Album", OFF),
+            ("beats", OFF),
+        ],
+    },
+]
+
+
+masterpiece = [
+    {
+        "group_control": NEUTRAL,
+        "label": "cowriter",
+        "scales": [
+            ("None", ON),
             ("track", NEUTRAL),
             ("cowriter", NEUTRAL),
             # ("2", NEUTRAL),
@@ -28,31 +193,21 @@ sections_data = [
         "group_control": NEUTRAL,
         "label": "Genesis",
         "scales": [
-            ("piano", NEUTRAL),
-            ("lyric-piano", NEUTRAL),
-            ("lyric", NEUTRAL),
-            ("title", NEUTRAL),
-
-            ("track", NEUTRAL),
-            ("mouth", OFF),
+            ("piano", NEUTRAL),            
+            ("track", NEUTRAL),            
             ("aku", NEUTRAL),
             ("dist", NEUTRAL),
             ("melody", NEUTRAL),
             ("clean", NEUTRAL),
+            ("lyric-piano", OFF),
+            ("lyric", OFF),
+            ("title", OFF),
+            ("mouth", OFF),
+            ("beat", OFF),
+
         ],
     },
-    # {
-    #     "group_control": NEUTRAL,
-    #     "label": "Fall",
-    #     "scales": [
-    #         ("5", NEUTRAL),
-    #         ("4", NEUTRAL),
-    #         ("3", NEUTRAL),
-    #         ("2", NEUTRAL),
-    #         ("1", NEUTRAL),
-    #     ],
-    # },
-    {
+        {
         "group_control": NEUTRAL,
         "label": "Masterpiece",
         "scales": [
@@ -74,20 +229,21 @@ sections_data = [
             ("bridge", NEUTRAL),
             ("instr", NEUTRAL),
             ("riff", NEUTRAL),
+            ("beat", OFF),
         ],
     },
     {
         "group_control": NEUTRAL,
         "label": "Feel",
         "scales": [
-            ("2", ON),
-            ("3", OFF),
-            ("4", OFF),
-            ("6", OFF),
-            ("shuffle", OFF),
-            ("half", OFF),
-            ("waltz", OFF),
-            ("triplet", OFF),
+            ("2", NEUTRAL),
+            ("3", NEUTRAL),
+            ("4", NEUTRAL),
+            ("6", NEUTRAL),
+            ("shuffle", NEUTRAL),
+            # ("half", OFF),
+            ("waltz", NEUTRAL),
+            ("triplet", NEUTRAL),
             # ("straight", NEUTRAL),
         ],
     },
@@ -150,3 +306,150 @@ sections_data = [
         ],
     },
 ]
+
+
+
+previous = [
+    {
+        "group_control": NEUTRAL,
+        "label": "cowriter",
+        "scales": [
+            ("None", ON),
+            ("track", NEUTRAL),
+            ("cowriter", NEUTRAL),
+            # ("2", NEUTRAL),
+            # ("1", NEUTRAL),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "jd",
+        "scales": [
+            ("5", NEUTRAL),
+            ("4", NEUTRAL),
+            ("3", NEUTRAL),
+            ("2", NEUTRAL),
+            ("1", NEUTRAL),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "Genesis",
+        "scales": [
+            ("piano", NEUTRAL),
+            
+            ("track", NEUTRAL),
+            
+            ("aku", NEUTRAL),
+            ("dist", NEUTRAL),
+            ("melody", OFF),
+            ("clean", NEUTRAL),
+            ("lyric-piano", OFF),
+            ("lyric", OFF),
+            ("title", OFF),
+            ("mouth", OFF),
+            ("beat", OFF),
+
+        ],
+    },
+        {
+        "group_control": NEUTRAL,
+        "label": "Masterpiece",
+        "scales": [
+            ("5", NEUTRAL),
+            ("4", NEUTRAL),
+            ("3", NEUTRAL),
+            ("2", NEUTRAL),
+            ("1", NEUTRAL),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "Section",
+        "scales": [
+            ("chorus", NEUTRAL),
+            ("verse", NEUTRAL),
+            ("pre", NEUTRAL),
+            ("post", NEUTRAL),
+            ("bridge", NEUTRAL),
+            ("instr", NEUTRAL),
+            ("riff", NEUTRAL),
+            ("beat", OFF),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "Feel",
+        "scales": [
+            ("2", ON),
+            ("3", OFF),
+            ("4", ON),
+            ("6", OFF),
+            ("shuffle", OFF),
+            # ("half", OFF),
+            ("waltz", OFF),
+            ("triplet", OFF),
+            # ("straight", NEUTRAL),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "FirstChord",
+        "scales": [
+            ("1", NEUTRAL),
+            ("6", NEUTRAL),
+            ("4", NEUTRAL),
+            ("5", NEUTRAL),
+            ("3", NEUTRAL),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "Melody",
+        "scales": [
+            ("3", NEUTRAL),
+            ("1", NEUTRAL),
+            ("6", NEUTRAL),
+            ("4", NEUTRAL),
+            ("2", NEUTRAL),
+            ("5", NEUTRAL),
+            ("7", NEUTRAL),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "Mode",
+        "scales": [
+            ("minor", NEUTRAL),
+            ("major", NEUTRAL),
+            ("mixo", OFF),
+            ("mixed", OFF),
+            ("phrygian", OFF),
+            ("lydian", OFF),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "melodyStart",
+        "scales": [
+            ("before", NEUTRAL),
+            ("on", NEUTRAL),
+            ("after", NEUTRAL),
+        ],
+    },
+    {
+        "group_control": NEUTRAL,
+        "label": "%_folderpath%",
+        "scales": [
+            ("ideas", NEUTRAL),
+            ("solo", OFF),
+            ("covers", OFF),
+            ("previous", OFF),
+            ("hold", OFF),
+            ("The Pink Album", OFF),
+            ("beats", OFF),
+        ],
+    },
+]
+
+sections_data = for_jake
